750ffadf4331b787d3b88fb59e7b1efbe51ebc0f
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-10-01 22:56:20 +0300

    chore: bot core development

--- src/bot/_application.py
+++ src/bot/_application.py
@@ -1,17 +1,49 @@
-from aiogram import Bot, Dispatcher, executor, types
+from typing import Any, Awaitable, List
+
+from aiogram import Bot, Dispatcher, executor
+from aiogram import types as AIOGramTypes
+from telegram import ReplyKeyboardMarkup
 
 from ._config import Config
 
 
 class Application:
     """ Класс-ядро приложения """
+
     __dispatcher: Dispatcher
 
     def __init__(self, config: Config) -> None:
         bot = Bot(token = config.get("Telegram", "TOKEN"))
         self.__dispatcher = Dispatcher(bot)
 
-    @classmethod
     def run(self) -> None:
-        """ Запуск приложения (бота) """
+        print("Running bot application...")
         executor.start_polling(self.__dispatcher, skip_updates = True)
+
+    def on(self, command: List[str], handler: Awaitable):
+        @self.__dispatcher.message_handler(commands = command or None)
+        async def call_handler(message: AIOGramTypes.Message):
+            await handler(message)
+
+    def create_keyboard_markup(
+        self,
+        items: List[str] = [],
+        resize_keyboard: bool or None = True,
+        one_time_keyboard: bool or None = False,
+        input_field_placeholder: Any or None = None,
+        selective: bool or None = None,
+        row_width: int or None = None,
+    ) -> ReplyKeyboardMarkup:
+        markup = AIOGramTypes.ReplyKeyboardMarkup(
+            resize_keyboard = resize_keyboard,
+            one_time_keyboard = one_time_keyboard,
+            input_field_placeholder = input_field_placeholder,
+            selective = selective,
+            row_width = row_width
+        )
+
+        for button_text in items:
+            keyboard_button = AIOGramTypes.KeyboardButton(button_text)
+            markup.add(keyboard_button)
+
+        return markup
--- src/bot/index.py
+++ src/bot/index.py
@@ -1,12 +1,36 @@
+from aiogram import types as AIOGramTypes
+
 from ._application import Application
 from ._config import create_config
 from ._constants import CONFIG_PATH
 
 config = create_config(CONFIG_PATH)
-
-print(config["Telegram"]["TOKEN"])
-
 bot = Application(config)
+
+async def start(message: AIOGramTypes.Message):
+    markup = bot.create_keyboard_markup(list = ["LeftButton", "RightButton"], resize_keyboard = False)
+
+    await message.reply("Hi!", reply_markup = markup)
+
+async def text_handler(message: AIOGramTypes.Message):
+    message_reply = ""
+
+    if(message.chat.type == "private"):
+        if(message.text == "LeftButton"):
+            message_reply = "Left button"
+        elif(message.text == "RightButton"):
+            message_reply = "Right button"
+
+    if(message_reply == ""):
+        message_reply = get_unknown_command_reply(message)
+
+    await message.answer(message_reply)
+
+def get_unknown_command_reply(message: AIOGramTypes.Message) -> str:
+    return f"Не удалось распознать команду {message.text}! Попробуйте команду /help, чтобы узнать, какие команды поддерживаются."
+
+bot.on(command = ["start", "hub"], handler = start)
+bot.on(command = [], handler = text_handler)
 
 def main():
     bot.run()
