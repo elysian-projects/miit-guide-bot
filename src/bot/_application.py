db33cf9792d531920368124e2cf460dec1ded76b
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-10-03 23:31:46 +0300

    feat(send_photo): add `send_photo` feature to the core module

--- src/bot/_application.py
+++ src/bot/_application.py
@@ -1,8 +1,7 @@
-from typing import Any, Awaitable, List, Literal
+from typing import Any, Awaitable, Coroutine, List
 
 from aiogram import Bot, Dispatcher, executor
 from aiogram import types as AIOGramTypes
-from telegram import ReplyKeyboardMarkup
 
 from ._config import Config
 
@@ -15,10 +14,14 @@ class Application:
     """
 
     __dispatcher: Dispatcher
+    __bot: Bot
 
     def __init__(self, config: Config) -> None:
         bot = Bot(token = config.get("Telegram", "TOKEN"))
-        self.__dispatcher = Dispatcher(bot)
+        dispatcher = Dispatcher(bot)
+
+        self.__dispatcher = dispatcher
+        self.__bot = bot
 
     def run(self) -> None:
         """ Запуск приложения (бота) """
@@ -59,7 +62,7 @@ class Application:
         one_time_keyboard: bool or None = None,
         input_field_placeholder: Any or None = None,
         selective: bool or None = None,
-    ) -> ReplyKeyboardMarkup:
+    ) -> AIOGramTypes.ReplyKeyboardMarkup:
         """
         Создаёт клавиатуру в меню (около поля ввода)
 
@@ -84,3 +87,43 @@ class Application:
             markup.add(keyboard_button)
 
         return markup
+
+    async def send_photo(
+        self,
+        message: AIOGramTypes.Message,
+        photo: AIOGramTypes.InputFile | str,
+        caption: str | None = None,
+        parse_mode: str | None = None,
+        disable_notification: bool | None = None,
+        protect_content: bool | None = None,
+        reply_to_message_id: int | None = None,
+        allow_sending_without_reply: bool | None = None,
+        reply_markup: AIOGramTypes.InlineKeyboardButton | AIOGramTypes.ReplyKeyboardMarkup | AIOGramTypes.ReplyKeyboardRemove | AIOGramTypes.ForceReply | None = None
+    ) -> Coroutine[Any, Any, AIOGramTypes.Message]:
+        """
+        (Асинхронная функция) Отправляет картинки в чат
+
+        @param message - объект, который принимает обработчик команд в качестве единственного параметра
+        @param photo - строковый путь до картинки (абсолютный)
+        @param caption - текст, который будет отправлен вместе с картинкой
+        @param parse_mode - Markdown/HTML
+        @param disable_notification - отправить сообщение в "бесшумном" режиме
+        @param protect_content - запретить пересылать и скачивать отправленный контент
+        @param reply_to_message_id - id оригинального сообщения, если команда является ответом
+        @param allow_sending_without_reply - отправить, даже если нет сообщения, на которое нужно ответить
+        @param reply_markup - клавиатура, которая будет отправлена вместе с сообщением
+
+        @see https://core.telegram.org/bots/api#sendphoto
+        """
+
+        await self.__bot.send_photo(
+            chat_id = message.chat.id,
+            photo = photo,
+            caption = caption,
+            parse_mode = parse_mode,
+            disable_notification = disable_notification,
+            protect_content = protect_content,
+            reply_to_message_id = reply_to_message_id,
+            allow_sending_without_reply = allow_sending_without_reply,
+            reply_markup = reply_markup
+        )
