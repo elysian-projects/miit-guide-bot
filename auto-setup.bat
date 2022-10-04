1a44aa2f529620f6f03b94477ca741c31a578ce5
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-10-04 13:29:24 +0300

    chore: add token.txt file

--- auto-setup.bat
+++ auto-setup.bat
@@ -4,5 +4,8 @@ python setup.py develop
 touch config.prod.ini
 touch config.dev.ini
 
+touch token.txt
+
 printf "[Telegram]\nTOKEN = " > config.prod.ini
 printf "[Telegram]\nTOKEN = " > config.dev.ini
+printf "В этом файле можно хранить тестовый токен бота. Файл не учитывается токеном" > token.txt
