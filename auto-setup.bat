c2abee9a3fc97541ff13c156d55d6a92af4fdae4
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-10-12 22:31:38 +0300

    fix: `auto-setup.bat` now loads the project packages with `pip`

--- auto-setup.bat
+++ auto-setup.bat
@@ -1,11 +1,11 @@
+pip install -U aiogram
+pip install -U pytest
+
 python setup.py install
 python setup.py develop
 
 touch config.prod.ini
 touch config.dev.ini
 
-touch token.txt
-
 printf "[Telegram]\nTOKEN = " > config.prod.ini
 printf "[Telegram]\nTOKEN = " > config.dev.ini
-printf "This file is created in order to store your local TOKEN. The file is not visible for git." > token.txt
