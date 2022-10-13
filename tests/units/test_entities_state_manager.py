c8ca089197a6691db4e5766f9131195449f1035b
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-10-14 00:02:50 +0300

    improve: test coverage
    * improve entities/database tests
    * improve entities/state_manager tests

--- tests/units/test_entities_state_manager.py
+++ tests/units/test_entities_state_manager.py
@@ -32,11 +32,90 @@ def test__set_location():
         state_manager.set_location("location1")
 
 
+def test__get_data_by_field():
+    state_manager = StateManager()
+
+    assert state_manager.get_data_by_field("name") == None
+
+    state_manager.set_points_list([{"name": "value1"}, {"name": "value2"}])
+    assert state_manager.get_data_by_field("name") == "value1"
+
+    state_manager.next_step()
+    assert state_manager.get_data_by_field("name") == "value2"
+
+    state_manager.next_step()
+    assert state_manager.get_data_by_field("name") == None
+
+    state_manager.reset_data()
+    assert state_manager.get_data_by_field("name") == None
+
+
+def test__set_points_list():
+    state_manager = StateManager()
+
+    assert state_manager.get_points_list() == []
+    assert len(state_manager.get_points_list()) == 0
+
+    state_manager.set_points_list(["value1", "value2"])
+
+    assert state_manager.get_points_list() == ["value1", "value2"]
+    assert len(state_manager.get_points_list()) == 2
+
+    state_manager.reset_data()
+    assert len(state_manager.get_points_list()) == 0
+
+
+def test__next_step():
+    state_manager = StateManager()
+
+    assert state_manager.get_current_step() == 0
+
+    state_manager.next_step()
+    assert state_manager.get_current_step() == 1
+
+    state_manager.next_step()
+    assert state_manager.get_current_step() == 2
+
+    state_manager.next_step()
+    state_manager.next_step()
+    assert state_manager.get_current_step() == 4
+
+
+def test__is_end():
+    state_manager = StateManager()
+
+    assert state_manager.is_end() == True
+
+    state_manager.set_points_list(["value1", "value2"])
+    assert state_manager.is_end() == False
+
+    state_manager.next_step()
+    assert state_manager.is_end() == False
+
+    state_manager.next_step()
+    assert state_manager.is_end() == True
+
+
+def test__is_location_chosen():
+    state_manager = StateManager()
+
+    assert state_manager.is_location_chosen() == False
+
+    state_manager.set_location("street")
+    assert state_manager.is_location_chosen() == True
+
+
 def test__reset_data():
     state_manager = StateManager()
+
+    state_manager.set_location("street")
+    state_manager.set_points_list(["value1", "value2"])
+    state_manager.next_step()
 
     state_manager.reset_data()
 
     assert state_manager.get_location() == None
     assert state_manager.is_location_chosen() == False
     assert state_manager.get_current_step() == 0
+    assert state_manager.get_points_list() == []
+    assert len(state_manager.get_points_list()) == 0
