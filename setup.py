140c1ca459fa80dba0a1fdca61f4959845fb13f6
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-11-07 20:33:28 +0300

    chore: add photos for `building_1`
    * add photos for the location
    * update version index

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "bot",
-    version = "0.1.0",
+    version = "1.0.0",
     packages = find_packages(exclude = ["tests", "tests.*", "docs"]),
     url = 'https://github.com/miit-projects/miit-guide-bot.git',
     python_requires='>=3.9',
