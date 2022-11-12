4f186d8ef1f8048c8626cfc481678c6e59d22d78
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-11-12 22:47:26 +0300

    chore(location): replace two simular functions with one template function

--- src/bot/utils/location.py
+++ src/bot/utils/location.py
@@ -1,24 +1,18 @@
-from ..constants.locations import AVAILABLE_LOCATIONS, Location
+from ..constants.locations import LOCATION_LABELS, LOCATION_VALUES, Locations
+from ..types.location import LocationProps
 
 
-def get_location_label(location: str) -> str | None:
-    for _, _location in Location.items():
-        if(location.strip() == _location["label"] or location.strip() == _location["value"]):
-            return _location["label"]
-    return None
-
-
-def get_location_value(location: str) -> str | None:
-    for _, _location in Location.items():
-        if(location.strip() == _location["label"] or location.strip() == _location["value"]):
-            return _location["value"]
+def get_location_property(location: str, prop: LocationProps) -> str | None:
+    for _, _location in Locations.items():
+        if(location.strip() == _location.label or location.strip() == _location.value):
+            return _location.label if prop == LocationProps.label else _location.value
     return None
 
 
 def is_valid_location(location: str) -> bool:
-    return location.strip() in [_location["value"] for _location in AVAILABLE_LOCATIONS] \
-        or location.strip() in [_location["label"] for _location in AVAILABLE_LOCATIONS]
+    return location.strip() in LOCATION_VALUES \
+        or location.strip() in LOCATION_LABELS
 
 
 def format_location_for_database(location: str) -> str | None:
-    return get_location_value(location)
+    return get_location_property(location, LocationProps.value)
