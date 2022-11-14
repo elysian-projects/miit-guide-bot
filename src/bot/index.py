93492132b19568ef8ddc81cd4a3650d54df05407
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-11-14 22:42:43 +0300

    chore: add telegram commands for development and debugging

--- src/bot/index.py
+++ src/bot/index.py
@@ -2,6 +2,7 @@ from ._application import AIOGramTypes, Application
 from .constants.keyboard import Keyboard
 from .constants.replies import Reply
 from .entities.database import Database
+from .env import IS_PRODUCTION
 from .types.buttons import Button
 from .utils.keyboard import *
 from .utils.location import *
@@ -50,11 +51,34 @@ async def text_handler(message: AIOGramTypes.Message):
     await bot.send_message(chat_id = message.chat.id, text = Reply.UNKNOWN_COMMAND)
 
 
-async def get_location(message: AIOGramTypes.Message):
+async def debug__get_location(message: AIOGramTypes.Message):
     """
-    DEBUG ONLY!!! REMOVE IN PRODUCTION
+    DEBUG ONLY!!! WON'T WORK IN PRODUCTION
     """
-    await bot.send_message(chat_id = message.chat.id, text = bot.state.get_location())
+    await bot.send_message(
+        chat_id = message.chat.id,
+        text = repr(bot.state.get_location())
+    )
+
+
+async def debug__get_location_data(message: AIOGramTypes.Message):
+    """
+    DEBUG ONLY!!! WON'T WORK IN PRODUCTION
+    """
+    await bot.send_message(
+        chat_id = message.chat.id,
+        text = f"[{format([_point for _point in bot.state.get_points_list()])}]"
+    )
+
+
+async def debug__get_current_point_data(message: AIOGramTypes.Message):
+    """
+    DEBUG ONLY!!! WON'T WORK IN PRODUCTION
+    """
+    await bot.send_message(
+        chat_id = message.chat.id,
+        text = format(bot.state.get_point_data())
+    )
 
 
 async def inline_keyboard_handler(call: AIOGramTypes.CallbackQuery):
@@ -117,7 +141,14 @@ async def end_of_excursion(message: AIOGramTypes.Message):
 
 
 bot.add_command_handler(command = ["start", "hub"], handler = start)
-bot.add_command_handler(command = ["get_location"], handler = get_location)
+
+# Команды для проверки состояния приложения в runtime.
+# Недоступны, если приложения находится в режиме `production` - `src/bot/env.py - IS_PRODUCTION = true`
+if(not IS_PRODUCTION):
+    bot.add_command_handler(command = ["get_location"], handler = debug__get_location)
+    bot.add_command_handler(command = ["get_location_data"], handler = debug__get_location_data)
+    bot.add_command_handler(command = ["get_point_data"], handler = debug__get_current_point_data)
+
 bot.add_command_handler(command = [], handler = text_handler)
 
 bot.add_inline_keyboard_handler(handler = inline_keyboard_handler)
