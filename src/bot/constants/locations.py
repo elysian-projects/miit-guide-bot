cfe09490cf4b0de45aa261bea58cd630e07b6776
Vladislav Zheltov <74919692+vladzheltov1@users.noreply.github.com>
2022-11-27 21:59:49 +0300

    Release 1.1.0 (#40)
    * chore(typing): improve typing of core elements, refactoring (#32)
    * chore(location): replace two similar functions with one template function
    * chore(typing): make objects that represent locations more clear in usage
    * chore(refactoring): improve extra links sending system
    * chore(refactoring): improve interaction with points inside of locations
    * improve typing
    * make object behavior clearer
    * remove unused and deprecated functionality
    * chore: refactoring
    * chore: move complicated conditions to functions
    * chore: add telegram commands for development and debugging
    * feat: send multiple pictures in one message (#31)
    * fix: incorrect display of extra links (#34)

--- src/bot/constants/locations.py
+++ src/bot/constants/locations.py
@@ -1,18 +1,13 @@
 from typing import List
 
-Location = {
-    "street": {
-        "label": "Улица",
-        "value": "street"
-    },
-    "building_1": {
-        "label": "Корпус 1",
-        "value": "building_1"
-    }
+from ..types.location import Location
+
+Locations = {
+    "street": Location("Улица", "street"),
+    "building_1": Location("Корпус 1", "building_1")
 }
 
+AVAILABLE_LOCATIONS: List[Location] = [Locations[location] for location in Locations]
 
-AVAILABLE_LOCATIONS: List[str] = [Location[location] for location in Location]
-
-LOCATION_LABELS = [_location["label"] for _location in AVAILABLE_LOCATIONS]
-LOCATION_VALUES = [_location["value"] for _location in AVAILABLE_LOCATIONS]
+LOCATION_LABELS = [_location.label for _location in AVAILABLE_LOCATIONS]
+LOCATION_VALUES = [_location.value for _location in AVAILABLE_LOCATIONS]
